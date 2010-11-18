285a08db8b49e4e310694395e1b5ed85bca75395
Mikhail Korobov <kmike84@gmail.com>
2010-11-18 11:42:16 +0500

    New-style class

--- facebook_connect/auth.py
+++ facebook_connect/auth.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User
 from facebook_connect.models import FacebookProfile
 
-class FacebookBackend:
+class FacebookBackend(object):
     def authenticate(self, uid=None):
         try:
             return FacebookProfile.objects.get(uid=uid).user
         except:
             return None
-        
+
     def get_user(self, user_id=None):
         try:
             return User.objects.get(pk=user_id)
