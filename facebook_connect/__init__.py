0bb19902c3d115799c8b8d50376f7ec491300bfb
Mikhail Korobov <kmike84@gmail.com>
2010-11-18 15:14:15 +0500

    New Facebook js API.
    Support for pre-filling user profile with email,
    first name and last name on registration (off by default).
    
    Login view is not updated.

--- facebook_connect/__init__.py
+++ facebook_connect/__init__.py
@@ -1,40 +1,35 @@
 from django.forms import Form
 from django.conf import settings
-
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import AnonymousUser, User
+from django.db.models import Q
 
 from registration import signals
+import facebook
 
-from facebook_connect.views import _verify_signature
 from facebook_connect.models import FacebookProfile
 import facebook_connect.settings
 
 class FacebookConnectBackend(object):
 
     def register(self, request, **kwargs):
-        params = _verify_signature(request.COOKIES)
-        if params and params['user'] != 'None':
-            uid = params['user']
+        params = facebook.get_user_from_cookie(
+            request.COOKIES,
+            facebook_connect.settings.FACEBOOK_APP_ID,
+            facebook_connect.settings.FACEBOOK_SECRET_KEY
+        )
+        if params:
+            uid = params['uid']
             try:
                 profile = FacebookProfile.objects.get(uid=uid)
-                user_obj = profile.user
             except FacebookProfile.DoesNotExist:
-                # Check that the username is unique, and if so, create a user and profile
-                try:
-                    existing_user = User.objects.get(username=uid)
-                    # Should we redirect here, or return False and redirect in post_registration_redirect?
+                profile = self.create_facebook_profile(uid, params)
+                # Should we redirect here, or return False and
+                # redirect in post_registration_redirect?
+                if not profile:
                     return False
-                except User.DoesNotExist:
-                    user_obj = User.objects.create_user(
-                        username=uid,
-                        email='',
-                        password=User.objects.make_random_password(16)
-                    )
-                    profile = FacebookProfile.objects.create(
-                        user=user_obj,
-                        uid=uid
-                    )
+
+            user_obj = profile.user
 
             signals.user_registered.send(
                 sender=self.__class__,
@@ -50,6 +45,19 @@ class FacebookConnectBackend(object):
             # Perhaps we should handle this differently?
             user_obj = AnonymousUser()
         return user_obj
+
+    def create_facebook_profile(self, uid, params):
+        # Check that the username is unique, and if so, create a user and profile
+        try:
+            existing_user = User.objects.get(username=uid)
+            return False
+        except User.DoesNotExist:
+            user_obj = User.objects.create_user(
+                username=uid,
+                email='',
+                password=User.objects.make_random_password(16)
+            )
+            return FacebookProfile.objects.create(user=user_obj, uid=uid)
 
     def registration_allowed(self, request):
         return facebook_connect.settings.REGISTRATION_OPEN
@@ -70,3 +78,39 @@ class FacebookConnectBackend(object):
 
     def post_activation_redirect(self, request, user):
         return NotImplementedError
+
+
+class FacebookConnectEmailBackend(FacebookConnectBackend):
+
+    def get_user_data(self, params):
+        # retrieve additional user info via facebook graph api
+        api = facebook.GraphAPI(params['access_token'])
+        try:
+            data = api.get_object('me')
+            # prevent fake email accounts creation
+            if len(data.get('email', '')) > 75:
+                data['email'] = None
+            return data
+        except facebook.GraphAPIError:
+            return {'email': None}
+
+
+    def create_facebook_profile(self, uid, params):
+        # Check that the username and email is unique,
+        # and if so, create a user and profile
+        assert 'email' in facebook_connect.settings.FACEBOOK_REQUIRED_FIELDS
+        user_data = self.get_user_data(params)
+        email = user_data['email']
+        try:
+            existing_user = User.objects.get(Q(username=uid) or Q(email=email))
+            return False
+        except User.DoesNotExist:
+            user = User.objects.create_user(
+                username=uid,
+                email=email,
+                password=User.objects.make_random_password(16)
+            )
+            user.first_name = user_data.get('first_name', '')
+            user.last_name = user_data.get('last_name', '')
+            user.save()
+            return FacebookProfile.objects.create(user=user, uid=uid)
--- facebook_connect/settings.py
+++ facebook_connect/settings.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 
+FACEBOOK_APP_ID = settings.FACEBOOK_APP_ID
 FACEBOOK_API_KEY = settings.FACEBOOK_API_KEY
 FACEBOOK_SECRET_KEY = settings.FACEBOOK_SECRET_KEY
-FACEBOOK_REQUIRED_FIELDS = getattr(settings, 'FACEBOOK_REQUIRED_FIELDS', ['email'])
+FACEBOOK_REQUIRED_FIELDS = getattr(settings, 'FACEBOOK_REQUIRED_FIELDS', [])
 REGISTRATION_OPEN = getattr(settings, 'REGISTRATION_OPEN', True)
 FACEBOOK_POST_REGISTRATION_REDIRECT = getattr(
     settings, 'FACEBOOK_POST_REGISTRATION_REDIRECT',
     getattr(settings, 'LOGIN_REDIRECT_URL', '/')
 )
+
+FACEBOOK_AUTH_BACKEND = getattr(settings, 'FACEBOOK_AUTH_BACKEND',
+                                'facebook_connect.FacebookConnectBackend')
